86922570bd73ec785161b4750f53941200cabf96
Khoawawa <atom08072004@gmail.com>
2025-02-28 14:33:48 +0700

    a

--- LBP.py
+++ LBP.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import glob
-import xgboost as xgb
+# import xgboost as xgb
 from skimage.feature import local_binary_pattern
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -12,20 +12,21 @@ class LBP():
         self.radius = R
         
 class LBP_XG_Model():
-    def __init__(self,P,R):
+    def __init__(self,P=8,R=1):
         self.lbp = LBP(P,R)
     def extract_LBP_feature(self,image):
-        lbp = local_binary_pattern(image,self.lbp.P,self.lbp.R,'uniform')
+        lbp = local_binary_pattern(image,self.lbp.point,self.lbp.radius,'uniform')
         lbp_hist, _ = np.histogram(lbp.ravel(), bins=np.arange(257), density=True)  
         return lbp_hist
-# class Hog():
-  
-def extract_hog_features(images):
-    feature_list = []
-    for img in images:
-        features = hog(img, orientations=9, pixels_per_cell=(8, 8),
-                       cells_per_block=(2, 2), block_norm='L2-Hys')
-        feature_list.append(features)
-    return np.array(feature_list)
 
 # def train_xg_boost(train_df)
+if __name__ == "__main__":
+    img = cv2.imread('test.jpg',0)
+    print("Image type:", type(img))
+    print("Image dtype:", img.dtype)
+    print("Image shape:", img.shape)
+    lbp_hist = LBP_XG_Model().extract_LBP_feature(img)
+    print(lbp_hist)
+    import matplotlib.pyplot as plt
+    plt.hist(lbp_hist,bins=256, color="gray", alpha=0.7)
+    plt.show()
